1692f142e0b86d3c81deafc1f1ff16ce3acc306d
peachmarrow12 <114831960+peachmarrow13@users.noreply.github.com>
2024-03-28 16:40:42 -0400

    Delete initials.cpp

--- initials.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-// Fun fact I am lazy af
-int main() {
-
-    cout << "K     K    D D D D\nK    K     D       D\nK   K      D       D\nK  K       D       D\nK K        D       D\nK  K       D       D\nK   K      D       D\nK    K     D       D\nK     K    D D D D\n";
-
-}
